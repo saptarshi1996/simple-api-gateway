a884712e16dc167282c6675552ad0dc75e9122bd
saptarshi1996 <saptarshisg96@gmail.com>
2022-07-16 21:31:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+const { PORT } = process.env
+
+const express = require('express')
+
+const app = express()
+
+
+app.listen(PORT, () => console.log(`Server on PORT ${PORT}`))
